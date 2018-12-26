48246e9db680287ce4fee6eac25ffe6f92b47df1
ayase <paper.airplane01@gmail.com>
2018-12-26 19:44:28 +0900

    Refactoring test config.

--- src/config.rs
+++ src/config.rs
@@ -1,38 +1,75 @@
-use std::path::Path;
 use std::io::{Read, BufReader};
 use std::fs::File;
-
+use std::marker::PhantomData;
+use std::default::Default;
 use serde_derive::Deserialize;
 
 // This is what we're going to decode into.
 #[derive(Deserialize)]
 pub struct Config {
-    pub bridge_ip_address: String,
-    pub username: String,
+    bridge_ip_address: String,
+    username: String,
 }
 
-// Parse config file.
-pub fn parse_config(config_path: &Path) -> Result<Config, failure::Error> {
-
-    let file = File::open(config_path)?;
-    let mut reader = BufReader::new(file);
-
-    // read into a String.
-    let mut buffer = String::new();
-    reader.read_to_string(&mut buffer)?;
-
-    let config = toml::from_str(&buffer)?;
-    Ok(config)
-}
-
-#[test]
-fn test_parse_config() {
-    fn test_config(config: Config) {
-        assert_eq!(config.bridge_ip_address, "192.168.1.10");
-        assert_eq!(config.username, "3AyHHXYqfsEaWTD102MLlDNeBiJkbuk6XY8YOqK1");
+impl Config {
+    pub fn bridge_ip_address(&self) -> &String {
+        &self.bridge_ip_address
     }
-    match parse_config(Path::new("./data/config.toml")) {
-        Ok(config) => test_config(config),
-        Err(error) => panic!("Error has occurred in test_parse_config(): {:?}", error),
-    };
+    pub fn username(&self) -> &String {
+        &self.username
+    }
+}
+
+pub struct Empty;
+pub struct Fully;
+pub struct ConfigBuilder<ConfigPath> {
+    config_path: String,
+    state: PhantomData<ConfigPath>
+}
+impl ConfigBuilder<Empty> {
+    pub fn new() -> ConfigBuilder<Empty> {
+        ConfigBuilder { 
+            config_path: Default::default(),
+            state: PhantomData,
+        }
+    }
+    pub fn config_path<S: Into<String>>(self, config_path: S) -> ConfigBuilder<Fully> {
+        ConfigBuilder {
+            config_path: config_path.into(),
+            state: PhantomData,
+        }
+    }
+}
+impl ConfigBuilder<Fully> {
+    // Parse config file.
+    pub fn build(self) -> Result<Config, failure::Error> {
+        let file = File::open(&self.config_path)?;
+        let mut reader = BufReader::new(file);
+
+        // read into a String.
+        let mut buffer = String::new();
+        reader.read_to_string(&mut buffer)?;
+
+        let config = toml::from_str(&buffer)?;
+        Ok(config)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::config::Config;
+    use crate::config::ConfigBuilder;
+
+    #[test]
+    fn test_parse_config() {
+        fn test_config(config: Config) {
+            assert_eq!(config.bridge_ip_address(), "192.168.1.10");
+            assert_eq!(config.username(), "3AyHHXYqfsEaWTD102MLlDNeBiJkbuk6XY8YOqK1");
+        }
+
+        match ConfigBuilder::new().config_path("./data/config.toml").build() {
+            Ok(config) => test_config(config),
+            Err(err) => panic!("Error has occurred in test_parse_config(): {:?}", err),
+        };
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,5 @@ extern crate serde_json;
 extern crate toml;
 extern crate reqwest;
 
-use std::path::Path;
-
 fn main() {
-    match my_home_hue::config::parse_config(Path::new("./data/config.toml")) {
-        Ok(config) => {
-            let bridge = my_home_hue::hue::bridge::BridgeBuilder::new()
-                            .ip_address(config.bridge_ip_address)
-                            .username(config.username)
-                            .build();
-
-            match bridge.get_light(1) {
-                Ok(body) => println!("{:#?}", body),
-                Err(error) => println!("{:?}", error),
-            }
-        },
-        Err(error) => println!("{:?}", error),
-    }
 }
