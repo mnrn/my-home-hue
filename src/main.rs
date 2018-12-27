be64223619bcccf4ac6c88a9ad50bdff0f23a6c1
ayase <paper.airplane01@gmail.com>
2018-12-28 00:28:39 +0900

    Refactoring- Separate functions into main and service.

--- src/main.rs
+++ src/main.rs
@@ -9,19 +9,36 @@ extern crate chrono;
 
 use std::collections::HashMap;
 use std::io::Write;
+use failure::Error;
 use serde_json::Value;
 use log::{LevelFilter, error, info};
 use env_logger::Builder;
 use chrono::Local;
 use my_home_hue::hue::bridge::BridgeBuilder;
 
-fn main() {
-    static BRIDGE_IP_ADDRESS: &'static str = "192.168.1.10";
-    static USERNAME: &'static str = "3AyHHXYqfsEaWTD102MLlDNeBiJkbuk6XY8YOqK1";
+// My home hue service.
+fn service() -> Result<(), Error> {
+    // Gets state of a my home hue light.
+    let bridge = BridgeBuilder::new()
+                            .ip_address("192.168.1.10")
+                            .username("3AyHHXYqfsEaWTD102MLlDNeBiJkbuk6XY8YOqK1")
+                            .build();
     const LIGHT_ID: u32 = 1;
-    const SCHEDULE_ID: u32 = 1;
+    let light = bridge.get_light(LIGHT_ID)?;
+    info!("{}", format!("Light/{} is {}", LIGHT_ID, if light.is_on() { "On" } else { "Off" }));
 
-    // Calls Builder::format to set a closure which formats each message text with timestamp.
+    // Change status of a schedule.
+    let schedule_status = if light.is_on() { "enabled" } else { "disabled" };
+    let body: HashMap<&str, &str> = [("status", schedule_status)].iter().cloned().collect();
+    const SCHEDULE_ID: u32 = 1;
+    let res: Value = bridge.set_schedule(SCHEDULE_ID, &body)?
+                        .json()?;
+    info!("{:#}", res);
+    Ok(())
+}
+
+fn main() {
+    // Call Builder::format to set a closure which formats each message text with timestamp.
     Builder::new()
         .format(|buf, record| {
             writeln!(buf, "{} [{}] - {}",
@@ -31,24 +48,8 @@ fn main() {
         .filter(None, LevelFilter::Info)
         .init();
 
-    // Gets state of a my home hue light.
-    let bridge = BridgeBuilder::new()
-                            .ip_address(BRIDGE_IP_ADDRESS)
-                            .username(USERNAME)
-                            .build();
-    let light = bridge.get_light(LIGHT_ID)
-                    .map_err(|err| error!("Error has occurred in Bridge::get_light: {:?}", err))
-                    .unwrap();
-    info!("{}", format!("Light/{} is {}", LIGHT_ID, if light.is_on() { "On" } else { "Off" }));
-
-    // Change status of a schedule.
-    let schedule_status = if light.is_on() { "enabled" } else { "disabled" };
-    let body: HashMap<&str, &str> = [("status", schedule_status)].iter().cloned().collect();
-    let res: Value = bridge.set_schedule(SCHEDULE_ID, &body)
-                        .map_err(|err| error!("Error has occurred in Bridge::set_schedule: {:?}", err))
-                        .unwrap()
-                        .json()
-                        .map_err(|err| error!("Error has occurred in reqwest::Response::json: {:?}", err))
-                        .unwrap();
-    info!("{:#?}", res);
+    // Execute my home hue service.
+    if let Err(err) = service() {
+        error!("Error has occurred while processing service: {}", err);
+    }
 }
