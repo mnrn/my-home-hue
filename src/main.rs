53f963768c053cc590c2513d7e081a613eae1b15
ayase <paper.airplane01@gmail.com>
2018-12-27 23:52:23 +0900

    Add comments.

--- src/main.rs
+++ src/main.rs
@@ -21,30 +21,31 @@ fn main() {
     const LIGHT_ID: u32 = 1;
     const SCHEDULE_ID: u32 = 1;
 
+    // Calls Builder::format to set a closure which formats each message text with timestamp.
     Builder::new()
         .format(|buf, record| {
             writeln!(buf, "{} [{}] - {}",
                 Local::now().format("%Y-%m-%dT%H:%M:%S"),
-                record.level(),
-                record.args()
-            )
+                record.level(), record.args())
         })
         .filter(None, LevelFilter::Info)
         .init();
 
+    // Gets state of a my home hue light.
     let bridge = BridgeBuilder::new()
                             .ip_address(BRIDGE_IP_ADDRESS)
                             .username(USERNAME)
                             .build();
     let light = bridge.get_light(LIGHT_ID)
-                    .map_err(|err| error!("Error has occurred in bridge.get_light: {:?}", err))
+                    .map_err(|err| error!("Error has occurred in Bridge::get_light: {:?}", err))
                     .unwrap();
     info!("{}", format!("Light/{} is {}", LIGHT_ID, if light.is_on() { "On" } else { "Off" }));
 
+    // Change status of a schedule.
     let schedule_status = if light.is_on() { "enabled" } else { "disabled" };
     let body: HashMap<&str, &str> = [("status", schedule_status)].iter().cloned().collect();
     let res: Value = bridge.set_schedule(SCHEDULE_ID, &body)
-                        .map_err(|err| error!("Error has occurred in bridge.set_schedule: {:?}", err))
+                        .map_err(|err| error!("Error has occurred in Bridge::set_schedule: {:?}", err))
                         .unwrap()
                         .json()
                         .map_err(|err| error!("Error has occurred in reqwest::Response::json: {:?}", err))
